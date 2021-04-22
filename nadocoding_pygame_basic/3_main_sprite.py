d2b7e12d8a36c45615ed3d79fb1651eb132798d1
Yongho Kim <digitaldna4@gmail.com>
2021-04-22 21:44:58 +0900

    Daily commit (22Apr2021)

---
+++ nadocoding_pygame_basic/3_main_sprite.py
@@ -0,0 +1,47 @@
+"""
+    나도코딩
+    활용편1
+    https://youtu.be/Dkx8Pl6QKW0
+"""
+
+import pygame
+
+pygame.init()   # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480
+screen_height = 640
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Hello Game")    # 게임 이름
+
+# 배경 이미지 불러오기
+background = pygame.image.load("D:\\Workspaces\\HelloWorld\\helloworld\\nadocoding_pygame_basic\\background.png")
+
+# 캐릭터(스프라이트) 불러오기
+character = pygame.image.load("D:\\Workspaces\\HelloWorld\\helloworld\\nadocoding_pygame_basic\\character.png")
+character_size = character.get_rect().size  # 이미지 크기를 구해옴
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width /2 )  # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로위치)
+character_y_pos = screen_height - character_height    # 화면 세로 크기의 가장 아래에 해당하는 곳에 위치 (세로위치)
+
+
+# 이벤트 루프
+running = True  # 게임이 진행중인가?
+while running:
+    #pass
+    for event in pygame.event.get():    # 어떤 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT:   # 창이 닫히는 이벤트가 발생하였는가?
+            running = False # 게임이 진행중이 아님
+    
+    screen.blit(background, (0,0))  # 배경 그리기
+    #screen.fill((0, 0, 255))
+
+    screen.blit(character, (character_x_pos, character_y_pos))
+
+    pygame.display.update() # 게임화면 다시 그리기
+
+# pygame 종료
+pygame.quit()
