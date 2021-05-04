153892a40375cbb2d50c9b5f5ead6aded13b2084
Yongho Kim <digitaldna4@gmail.com>
2021-05-04 23:40:11 +0900

    Daily commit (04May2021)

---
+++ nadocoding_gui_basic/1_create_frame.py
@@ -0,0 +1,16 @@
+"""
+    나도코딩
+    활용편2
+    https://youtu.be/bKPIcoou9N8
+"""
+
+from tkinter import *
+
+root = Tk()
+root.title("Hello GUI")
+root.geometry("600x480")
+#root.geometry("600x480+300+100")    # 가록 * 세로 + x좌표 + y좌표
+root.resizable(False, False)        # x(너비), y(너비) 값 변경 불가 (창 크리 변경 불가) / 최대화 아이콘도 비활성화
+
+
+root.mainloop()
