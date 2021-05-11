4a3210dd4df5ad54a882361739e298c1820f7efd
Yongho Kim <digitaldna4@gmail.com>
2021-05-11 21:48:30 +0900

    Daily commit (11May2021)

--- nadocoding_gui_basic/6_checkbox.py
+++ nadocoding_gui_basic/6_checkbox.py
@@ -12,13 +12,18 @@ root.geometry("640x480")
 
 chkvar = IntVar()   # chkvar에 int형으로 값을 저장
 checkbox = Checkbutton(root, text="오늘하루 보지 않기", variable=chkvar)
-checkbox.select()
-checkbox.deselect()
+# checkbox.select()
+# checkbox.deselect()
 checkbox.pack()
 
-def btncmd():
-    pass
+chkvar2 = IntVar() 
+checkbox2 = Checkbutton(root, text="일주일동안 보지 않기", variable=chkvar2)
+checkbox2.pack()
 
+def btncmd():
+    print(chkvar.get()) # 0: 체크해제, 1: 체크
+    print(chkvar2.get())
+    
 btn = Button(root, text="클릭", command=btncmd)
 btn.pack()
 
