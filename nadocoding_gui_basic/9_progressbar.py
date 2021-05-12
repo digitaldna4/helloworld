4055beef2b74c3455ad67ad010b0b2adfedd7c1d
Yongho Kim <digitaldna4@gmail.com>
2021-05-12 23:26:15 +0900

    Daily commit (12May2021)

---
+++ nadocoding_gui_basic/9_progressbar.py
@@ -0,0 +1,42 @@
+"""
+    나도코딩
+    활용편2
+    https://youtu.be/bKPIcoou9N8
+"""
+
+import tkinter.ttk as ttk
+from tkinter import *
+from typing import ValuesView
+import time
+
+root = Tk()
+root.title("Hello GUI")
+root.geometry("640x480")
+
+# #progressbar = ttk.Progressbar(root, maximum=100, mode="indeterminate")
+# progressbar = ttk.Progressbar(root, maximum=100, mode="determinate")
+# progressbar.start(10)       # 10ms 마다 움직임
+# progressbar.pack()
+
+# def btncmd():
+#     progressbar.stop()
+
+# btn = Button(root, text="중지", command=btncmd)
+# btn.pack()
+
+p_var2 = DoubleVar()
+progressbar2 = ttk.Progressbar(root, maximum=100, length=150, variable=p_var2)
+progressbar2.pack()
+
+
+def btncmd2():
+    for i in range(1, 100):
+        time.sleep(0.01)    #0.01초 대기
+        p_var2.set(i)
+        progressbar2.update()
+        print(p_var2.get())
+
+btn = Button(root, text="시작", command=btncmd2)
+btn.pack()
+
+root.mainloop()
