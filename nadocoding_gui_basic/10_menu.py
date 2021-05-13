d64a818e9ec3b587930eb7f033a33dbc6a08ad22
Yongho Kim <digitaldna4@gmail.com>
2021-05-13 22:52:35 +0900

    Daily commit (13May2021)

---
+++ nadocoding_gui_basic/10_menu.py
@@ -0,0 +1,48 @@
+"""
+    나도코딩
+    활용편2
+    https://youtu.be/bKPIcoou9N8
+"""
+
+from tkinter import *
+
+root = Tk()
+root.title("Hello GUI")
+root.geometry("640x480")
+
+def create_new_file():
+    print("새 파일을 만듭니다.")
+
+menu = Menu(root)
+
+# File 메뉴
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="New File", command=create_new_file)
+menu_file.add_command(label="New Window")
+menu_file.add_separator()
+menu_file.add_command(label="Open File...")
+menu_file.add_separator()
+menu_file.add_command(label="Save All", state="disable")
+menu_file.add_separator()
+menu_file.add_command(label="Exit", command=root.quit)
+menu.add_cascade(label="File", menu=menu_file)
+
+# Edit 메뉴
+menu.add_cascade(label="Edit")
+
+# Lanugage 메뉴 추가 (radio 버튼을 통해서 택1)
+menu_lang = Menu(menu, tearoff=0)
+menu_lang.add_radiobutton(label="Python")
+menu_lang.add_radiobutton(label="Java")
+menu_lang.add_radiobutton(label="C++")
+menu.add_cascade(label="Lanugage", menu=menu_lang)
+
+# View 메뉴 추가 (radio 버튼을 통해서 택1)
+menu_view = Menu(menu, tearoff=0)
+menu_view.add_checkbutton(label="Show Minimap")
+menu_view.add_checkbutton(label="Show Minimap2")
+menu.add_cascade(label="View", menu=menu_view)
+
+root.config(menu=menu)
+
+root.mainloop()
