69dac47ff3a04e2fb83963941027561849a0f370
Yongho Kim <digitaldna4@gmail.com>
2021-06-18 23:13:42 +0900

    Daily commit (18Jun2021)

--- nadocoding_webscraping_basic/14_selenium_flight.py
+++ nadocoding_webscraping_basic/14_selenium_flight.py
@@ -4,8 +4,11 @@
     https://youtu.be/yQ20jZwDjTE
 """
 
-import time
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome("d:/Workspaces/HelloWorld/helloworld/chromedriver_win32/chromedriver.exe")
 browser.maximize_window()   # 창 최대화
@@ -34,6 +37,13 @@ browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
 # 항공권 검색
 browser.find_element_by_link_text("항공권 검색").click()
 
-# 첫번째 결과 출력
-elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
-print(elem.text)
+try:
+    elem = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
+    print(elem.text)
+
+finally:
+    browser.quit()
+
+# # 첫번째 결과 출력
+# elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
+# print(elem.text)
